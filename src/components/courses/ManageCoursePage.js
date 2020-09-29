18dd0ff129fb387828c42e393b33d1434be619cf
Krzysztof <krzysztof.mohr@live.com>
2020-09-29 23:04:59 +0100

    added client side validation function#

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -43,6 +43,19 @@ function ManageCoursePage({courses, authors, loadAuthors, loadCourses, saveCours
             }));
         }
 
+        function formIsValid() {
+            const {title, authorId, category} = course;
+            const errors = {};
+
+            if(!title) errors.title = "Title is required";
+            if(!authorId) errors.author = "Author is required";
+            if(!category) errors.category = "Category is required";
+
+            setErrors(errors);
+
+            return Object.keys(errors).length === 0;
+        }
+
         function handleSave(event) {
             event.preventDefault();
             setSaving(true);
